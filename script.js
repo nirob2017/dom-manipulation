bf1c971df59493f947cf2bd32697fdadd01603d5
Md Monsur Hossain Nirob <nirob726@gmail.com>
2018-05-30 04:50:18 +0000

    Task_2a_and_task_2b

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 /* Task 1 */
-
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
+/*Task 2a */
+var textBtn = document.getElementById('textBtn2')
+textBtn.addEventListener('click',function(){
+    var task2 = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "Hello World"
+    task2.appendChild(paragraph)
+})
 
+//task 2b
+function changeBGColor(event){
+    var body = document.getElementsByTagName('body')[0]
+    if(event.target.innerText == "Red"){
+        body.style.backgroundColor = 'red'
+    }
+    if(event.target.innerText == "Green"){
+        body.style.backgroundColor = 'green'
+    }
+}
 
-
+//task 2c
+function redirectToGoogle(){
+    window.location.href = "https://www.google.com/";
+}
 /* Task 4 using jQuery */
